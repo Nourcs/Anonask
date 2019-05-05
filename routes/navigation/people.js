cf0ce48bef8492a93a7e583f667202492bad4a08
Nour Essoussi <nourcherifsoussi@gmail.com>
2019-05-05 11:54:37 -0400

    Ability to ask people.

--- routes/navigation/people.js
+++ routes/navigation/people.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var cookieParser = require("cookie-parser");
 
 var User = require("../../models/user");
-
+var Question = require("../../models/questions");
 var mongoose = require("mongoose");
 
 // Old User Log In
@@ -14,11 +14,28 @@ router.get("/people", (req, res, next) => {
 router.post("/people/search", (req, res, next) => {
   let name = req.body.name;
   console.log(name);
-  User.find({ fullName: new RegExp(name, "gi") })
-  .then(result => {
+  User.find({ fullName: new RegExp(name, "gi") }).then(result => {
     res.json(result);
   });
   // res.send("asba");
+});
+
+router.get("/people/:id", (req, res, next) => {
+  User.findById(req.params.id).then(user => {
+    res.render("navigation/peopleProfile", { user });
+  });
+});
+
+router.post("/people/:id/newQuestion", (req, res, next) => {
+  User.findById(req.params.id).then(user => {
+    let from = req.cookies.currentUser;
+    let to = user;
+    let question = req.body.question;
+    Question.create({ from, to, question }).then(question => {
+      console.log(user);
+    });
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
